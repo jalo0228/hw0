cede6b0b8671124e134164f1f3c91716bb467ed4
Youhyun <1486asdf@gmail.com>
2020-08-27 18:39:17 +0000

    add a period

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 temparature = input("Enter temperature in celsius:")
 temparature = float(temparature)
-print(f" {temparature}\N{degree sign} in Celsius is equivalent to {temparature*1.8+32}\N{degree sign} Fahrenheit")
+print(f" {temparature}\N{degree sign} in Celsius is equivalent to {temparature*1.8+32}\N{degree sign} Fahrenheit.")
