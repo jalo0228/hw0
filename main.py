d2624fd2b04e04ec35595f77a18573588b58dd47
Youhyun <1486asdf@gmail.com>
2020-08-27 18:13:22 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+temparature = input("Enter temperature in celsius: ")
+temparature = int(temparature)
+print(f"{temparature}\N{degree sign} in celsius is equivalent to {temparature*1.8+32}\N{degree sign}Fahrenheit")
